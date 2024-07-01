fadaeda54cea6665d9c90ee6b5b65cb821fddc60
EtielOguh <Hugodsleite@gmail.com>
2024-07-01 11:38:24 -0300

    Lvl

--- Monstros/Gosma.py
+++ Monstros/Gosma.py
@@ -2,6 +2,6 @@ from Inimigo import *
 
 class Gosma(Inimigo):
     def __init__(self):
-        super().__init__('Gosma', 50, 8, 100, 100, 'SpriteInimigo/Gosma.png')
+        super().__init__('Gosma', 1, 8, 100, 100, 'SpriteInimigo/Gosma.png')
 
 
--- Monstros/Lanceiro.py
+++ Monstros/Lanceiro.py
@@ -2,6 +2,6 @@ from Inimigo import *
 
 class Lanceiro(Inimigo):
     def __init__(self):
-        super().__init__('Lanceiro', 50, 8, 100, 100, 'SpriteInimigo/Lanceiro.png')
+        super().__init__('Lanceiro', 1, 8, 100, 100, 'SpriteInimigo/Lanceiro.png')
 
 
